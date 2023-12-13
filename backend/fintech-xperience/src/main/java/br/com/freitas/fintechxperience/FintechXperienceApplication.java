fd493e56c16d6019bd49af69ae7abfd051ed5d4b
Matheus Freitas <mf022rj@gmail.com>
2023-12-13 19:38:10 -0300

    Backend

---
+++ backend/fintech-xperience/src/main/java/br/com/freitas/fintechxperience/FintechXperienceApplication.java
@@ -0,0 +1,13 @@
+package br.com.freitas.fintechxperience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechXperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechXperienceApplication.class, args);
+	}
+
+}
---
+++ backend/fintech-xperience/src/test/java/br/com/freitas/fintechxperience/FintechXperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.freitas.fintechxperience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechXperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
